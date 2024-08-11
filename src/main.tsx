dc4872791d1b56270b1b1f8753739a6f77595759
javicod92 <javi.javier.jc@gmail.com>
2024-08-11 15:52:52 -0300

    adding NavBar Component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/main.css";
 import Header from "./components/Header/Header.tsx";
+import NavBar from "./components/NavBar/NavBar.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.Fragment>
     <Header />
+    <NavBar />
   </React.Fragment>
 );
