3d586d47eead5425a395764107d0ff406e7147e7
javicod92 <javi.javier.jc@gmail.com>
2024-08-13 11:56:11 -0300

    corrections

--- src/components/SoundFunction/SoundFunction.tsx
+++ src/components/SoundFunction/SoundFunction.tsx
@@ -3,9 +3,27 @@ import styles from "./SoundFunction.module.css";
 export default function SoundFunction() {
   return (
     <div className={styles.function_container}>
-      <img src="src\assets\static\svgs\repeat.svg" alt="Repeat" />
-      <img src="src\assets\static\svgs\volume_up.svg" alt="Volume" />
-      <img src="src\assets\static\svgs\arrow_drop_down.svg" alt="Drop Down" />
+      <button>
+        <img
+          className={styles.function_img}
+          src="src\assets\static\svgs\repeat.svg"
+          alt="Repeat"
+        />
+      </button>
+      <button>
+        <img
+          className={styles.function_img}
+          src="src\assets\static\svgs\volume_up.svg"
+          alt="Volume"
+        />
+      </button>
+      <button>
+        <img
+          className={styles.function_img}
+          src="src\assets\static\svgs\arrow_drop_down.svg"
+          alt="Drop Down"
+        />
+      </button>
     </div>
   );
 }
