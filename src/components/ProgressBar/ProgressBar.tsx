3d1bec019721b672049dd2edad83f5054d7f057f
javicod92 <javi.javier.jc@gmail.com>
2024-08-13 15:55:06 -0300

    Finishing Progress Bar

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,16 +1,24 @@
 import styles from "./ProgressBar.module.css";
+import { useState } from "react";
 
 export default function ProgressBar() {
+  const [value, setValue] = useState<number>(0);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(Number(event.target.value));
+  };
+
   return (
     <div className={styles.progresBar_container}>
-      <input onChange={ProgressBar}
+      <input
+        onChange={handleChange}
         className={styles.progressRange}
         type="range"
         id="progressRange"
         min="0"
         max="100"
-        value="0"
-        step="1"
+        value={value}
+        step=".1"
       />
     </div>
   );
