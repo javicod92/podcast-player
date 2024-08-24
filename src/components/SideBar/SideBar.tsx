1efb1888c4c6465dd3a50fb921fb33a492473ad4
javicod92 <javi.javier.jc@gmail.com>
2024-08-24 20:14:07 -0300

    adding automatic resizing to the PrincipalContent when the sidebar toggles between compact and expanded mode

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -13,67 +13,71 @@ export default function SideBar({ size = "compact" }: Props) {
 
   if (size === "compact") {
     return (
-      <div className={`${styles.navBar_container} ${styles.compact_container}`}>
-        <div className={styles.compact_sidebar}>
-          <MenuItem
-            primarytext="Principal"
-            buttonSize="vertically"
-            leftIconUrl="src/assets/static/svgs/house.svg"
-            isSelected
-          />
-          <MenuItem
-            primarytext="Explorar"
-            buttonSize="vertically"
-            leftIconUrl="src/assets/static/svgs/explore.svg"
-          />
-          <MenuItem
-            primarytext="Biblioteca"
-            buttonSize="vertically"
-            leftIconUrl="src/assets/static/svgs/library_music.svg"
-          />
+      <div className={styles.sidebar_compact_fill}>
+        <div className={`${styles.navBar_container} ${styles.compact_container}`}>
+          <div className={styles.compact_sidebar}>
+            <MenuItem
+              primarytext="Principal"
+              buttonSize="vertically"
+              leftIconUrl="src/assets/static/svgs/house.svg"
+              isSelected
+            />
+            <MenuItem
+              primarytext="Explorar"
+              buttonSize="vertically"
+              leftIconUrl="src/assets/static/svgs/explore.svg"
+            />
+            <MenuItem
+              primarytext="Biblioteca"
+              buttonSize="vertically"
+              leftIconUrl="src/assets/static/svgs/library_music.svg"
+            />
+          </div>
         </div>
       </div>
     );
   }
 
   return (
-    <div className={`${styles.navBar_container} ${styles.expanded_container}`}>
-      <div className={styles.expanded_sidebar}>
-        <div className={styles.sidebar_content_buttons}>
-          <MenuItem
-            primarytext="Principal"
-            buttonSize="horizontally"
-            leftIconUrl="src/assets/static/svgs/house.svg"
-            hasRightIcon
-            isSelected
-          />
-          <MenuItem
-            primarytext="Explorar"
-            buttonSize="horizontally"
-            leftIconUrl="src/assets/static/svgs/explore.svg"
-          />
-          <MenuItem
-            primarytext="Biblioteca"
-            buttonSize="horizontally"
-            leftIconUrl="src/assets/static/svgs/library_music.svg"
-          />
-        </div>
-      </div>
-      <div className={styles.playList_button_container}>
-        <Buttons text="Nueva Playlist" iconUrl="src/assets/static/svgs/icon.svg" isSelected/>
-      </div>
-      <div className={styles.playlist_list_container}>
-        <div className={styles.playlist_content}>
-        {userPlaylist.map(list => {
-          return (
-            <MenuItem 
-              key={list.key}
-              primarytext={list.playlistName}
-              secondaryText={list.createdBy}
+    <div className={styles.sidebar_expanded_fill}>
+      <div className={`${styles.navBar_container} ${styles.expanded_container}`}>
+        <div className={styles.expanded_sidebar}>
+          <div className={styles.sidebar_content_buttons}>
+            <MenuItem
+              primarytext="Principal"
               buttonSize="horizontally"
+              leftIconUrl="src/assets/static/svgs/house.svg"
+              hasRightIcon
+              isSelected
             />
-          )
-        })}
+            <MenuItem
+              primarytext="Explorar"
+              buttonSize="horizontally"
+              leftIconUrl="src/assets/static/svgs/explore.svg"
+            />
+            <MenuItem
+              primarytext="Biblioteca"
+              buttonSize="horizontally"
+              leftIconUrl="src/assets/static/svgs/library_music.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.playList_button_container}>
+          <Buttons text="Nueva Playlist" iconUrl="src/assets/static/svgs/icon.svg" isSelected/>
+        </div>
+        <div className={styles.playlist_list_container}>
+          <div className={styles.playlist_content}>
+          {userPlaylist.map(list => {
+            return (
+              <MenuItem 
+                key={list.key}
+                primarytext={list.playlistName}
+                secondaryText={list.createdBy}
+                buttonSize="horizontally"
+              />
+            )
+          })}
+          </div>
         </div>
       </div>
     </div>
