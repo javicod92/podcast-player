cfc64db585c33a5379461a3d3974347253e15e13
javicod92 <javi.javier.jc@gmail.com>
2024-09-29 16:06:35 -0300

    Improving styles for Progressbar (Slider) component

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import styles from "./ProgressBar.module.css";
 
 type Props = {
@@ -6,9 +7,27 @@ type Props = {
 };
 
 export default function ProgressBar(props: Props) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    function updateProgressBar() {
+      if (inputRef.current) {
+        const value = Number(inputRef.current.value);
+        const min = Number(inputRef.current.min);
+        const max = Number(inputRef.current.max);
+        const percentage = ((value - min) / (max - min)) * 100;
+
+        inputRef.current.style.background = `linear-gradient(to right, red ${percentage}%, #555 ${percentage}%)`;
+      }
+    }
+
+    updateProgressBar();
+  }, [props.progress]);
+
   return (
     <div className={styles.progresBar_container}>
       <input
+        ref={inputRef}
         onChange={props.onProgressChange}
         className={styles.progressRange}
         type="range"
