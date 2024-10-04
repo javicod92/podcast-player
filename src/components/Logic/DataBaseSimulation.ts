af2c77102128cbb3fa9e3037d2b76dfc90ce1970
javicod92 <javi.javier.jc@gmail.com>
2024-10-04 15:22:21 -0300

    Revert "Updating the static content path from “SRC” to “PUBLIC” to correct errors in the production stage"
    This reverts commit 51f5829afcc8ec0bbbad968e863f3ae05af9eeb2.

--- src/components/Logic/DataBaseSimulation.ts
+++ src/components/Logic/DataBaseSimulation.ts
@@ -56,7 +56,7 @@ export const ListenAgain = [
     key: 1,
     isAlbum: true,
     imageSource:
-      "/assets/static/images/albumsImage/07e12ccaff200ac1214f42ac2c466f1a.png",
+      "src/assets/static/images/albumsImage/07e12ccaff200ac1214f42ac2c466f1a.png",
     artist: "Various",
     playlistName: "Top 100 Argentina",
     songName: "none",
@@ -67,7 +67,7 @@ export const ListenAgain = [
     key: 2,
     isAlbum: false,
     imageSource:
-      "/assets/static/images/albumsImage/92d2f58023ddb67411ab73f18a6358c4.png",
+      "src/assets/static/images/albumsImage/92d2f58023ddb67411ab73f18a6358c4.png",
     artist: "Boris Brejcha",
     playlistName: "none",
     songName: "The Final",
@@ -78,7 +78,7 @@ export const ListenAgain = [
     key: 3,
     isAlbum: true,
     imageSource:
-      "/assets/static/images/albumsImage/58b9b28e29d52bd7f484b799db3341f6.png",
+      "src/assets/static/images/albumsImage/58b9b28e29d52bd7f484b799db3341f6.png",
     artist: "The Weeknd",
     playlistName: "Starboy",
     songName: "none",
@@ -89,7 +89,7 @@ export const ListenAgain = [
     key: 4,
     isAlbum: false,
     imageSource:
-      "/assets/static/images/albumsImage/758c2683a632d5521796b21be90d9541.png",
+      "src/assets/static/images/albumsImage/758c2683a632d5521796b21be90d9541.png",
     artist: "Ania Szarmach",
     playlistName: "none",
     songName: "Shades of Love",
@@ -100,7 +100,7 @@ export const ListenAgain = [
     key: 5,
     isAlbum: true,
     imageSource:
-      "/assets/static/images/albumsImage/42436ccdebd6e39a8dfe9b8fefa6d142.png",
+      "src/assets/static/images/albumsImage/42436ccdebd6e39a8dfe9b8fefa6d142.png",
     artist: "Roosevelt",
     playlistName: "Midnight Versions",
     songName: "none",
@@ -111,7 +111,7 @@ export const ListenAgain = [
     key: 6,
     isAlbum: true,
     imageSource:
-      "/assets/static/images/albumsImage/3b02630be867dbdbd629e9d32572a9b5.png",
+      "src/assets/static/images/albumsImage/3b02630be867dbdbd629e9d32572a9b5.png",
     artist: "Liking Park",
     playlistName: "Hybrid Theory",
     songName: "none",
@@ -121,7 +121,7 @@ export const ListenAgain = [
   {
     key: 7,
     isAlbum: false,
-    imageSource: "/assets/static/images/albumsImage/THR3MIND-Bumblebee.jpg",
+    imageSource: "src/assets/static/images/albumsImage/THR3MIND-Bumblebee.jpg",
     artist: "THR3MIND",
     playlistName: "Hybrid Theory",
     songName: "Bumblebee",
@@ -131,7 +131,7 @@ export const ListenAgain = [
   {
     key: 8,
     isAlbum: true,
-    imageSource: "/assets/static/images/albumsImage/FREYA-Ruback.jpg",
+    imageSource: "src/assets/static/images/albumsImage/FREYA-Ruback.jpg",
     artist: "FREYA",
     playlistName: "Ruback",
     songName: "none",
@@ -143,56 +143,56 @@ export const ListenAgain = [
 export const similarTo = [
   {
     key: 1,
-    imageUrl: "/assets/static/images/albumsImage/Boris1.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/Boris1.jpg",
     isAlbum: false,
     artist: "Boris Brejcha",
     subscriberNumber: 380000,
   },
   {
     key: 2,
-    imageUrl: "/assets/static/images/albumsImage/KyleWatson.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/KyleWatson.jpg",
     isAlbum: false,
     artist: "Kyle Watson",
     subscriberNumber: 2500,
   },
   {
     key: 3,
-    imageUrl: "/assets/static/images/albumsImage/Hozo.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/Hozo.jpg",
     isAlbum: false,
     artist: "Hozo",
     subscriberNumber: 2000000,
   },
   {
     key: 4,
-    imageUrl: "/assets/static/images/albumsImage/EnricoSangiuliano.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/EnricoSangiuliano.jpg",
     isAlbum: false,
     artist: "Enrico Sangiuliano",
     subscriberNumber: 35000,
   },
   {
     key: 5,
-    imageUrl: "/assets/static/images/albumsImage/AmbientPino.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/AmbientPino.jpg",
     isAlbum: false,
     artist: "Ambient Pino",
     subscriberNumber: 500,
   },
   {
     key: 6,
-    imageUrl: "/assets/static/images/albumsImage/bookaShade.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/bookaShade.jpg",
     isAlbum: false,
     artist: "Booka Shade",
     subscriberNumber: 50000,
   },
   {
     key: 7,
-    imageUrl: "/assets/static/images/albumsImage/ARTBAT.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/ARTBAT.jpg",
     isAlbum: false,
     artist: "ARTBAT",
     subscriberNumber: 12000,
   },
   {
     key: 8,
-    imageUrl: "/assets/static/images/albumsImage/THR3MIND-Bumblebee.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/THR3MIND-Bumblebee.jpg",
     isAlbum: false,
     artist: "THR3MIND",
     subscriberNumber: 22000,
@@ -213,7 +213,7 @@ export const quickPicks: quickPicksProps[] = [
   {
     key: 1,
     size: "small",
-    imageUrl: "/assets/static/images/albumsImage/KREAM-Pressure.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/KREAM-Pressure.jpg",
     artist: "KREAM",
     songTitle: "Pressure",
     views: 5000,
@@ -223,7 +223,7 @@ export const quickPicks: quickPicksProps[] = [
     key: 2,
     size: "small",
     imageUrl:
-      "/assets/static/images/albumsImage/07e12ccaff200ac1214f42ac2c466f1a.png",
+      "src/assets/static/images/albumsImage/07e12ccaff200ac1214f42ac2c466f1a.png",
     artist: "RYAN JONES",
     songTitle: "Pain",
     views: 270000,
@@ -232,7 +232,7 @@ export const quickPicks: quickPicksProps[] = [
   {
     key: 3,
     size: "small",
-    imageUrl: "/assets/static/images/albumsImage/Roosevelt-nightMoves.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/Roosevelt-nightMoves.jpg",
     artist: "Roosevelt",
     songTitle: "Night Moves",
     views: 560000,
@@ -242,7 +242,7 @@ export const quickPicks: quickPicksProps[] = [
     key: 4,
     size: "small",
     imageUrl:
-      "/assets/static/images/albumsImage/58b9b28e29d52bd7f484b799db3341f6.png",
+      "src/assets/static/images/albumsImage/58b9b28e29d52bd7f484b799db3341f6.png",
     artist: "The Weeknd",
     songTitle: "Starboy",
     views: 25000000,
@@ -252,7 +252,7 @@ export const quickPicks: quickPicksProps[] = [
     key: 5,
     size: "small",
     imageUrl:
-      "/assets/static/images/albumsImage/758c2683a632d5521796b21be90d9541.png",
+      "src/assets/static/images/albumsImage/758c2683a632d5521796b21be90d9541.png",
     artist: "Ania Szarmach",
     songTitle: "Shades of Love",
     views: 3000,
@@ -261,7 +261,7 @@ export const quickPicks: quickPicksProps[] = [
   {
     key: 6,
     size: "small",
-    imageUrl: "/assets/static/images/albumsImage/AmbientPino.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/AmbientPino.jpg",
     artist: "Ambient Pino",
     songTitle: "Yes Yes Yes",
     views: 170000,
@@ -270,7 +270,7 @@ export const quickPicks: quickPicksProps[] = [
   {
     key: 7,
     size: "small",
-    imageUrl: "/assets/static/images/albumsImage/Andromeda-Gorillaz.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/Andromeda-Gorillaz.jpg",
     artist: "Gorillaz",
     songTitle: "Andromeda",
     views: 560000,
@@ -279,7 +279,7 @@ export const quickPicks: quickPicksProps[] = [
   {
     key: 8,
     size: "small",
-    imageUrl: "/assets/static/images/albumsImage/ARTBAT.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/ARTBAT.jpg",
     artist: "ARTBAT",
     songTitle: "Melodic Techno",
     views: 270000000,
@@ -288,7 +288,7 @@ export const quickPicks: quickPicksProps[] = [
   {
     key: 9,
     size: "small",
-    imageUrl: "/assets/static/images/albumsImage/bookaShade.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/bookaShade.jpg",
     artist: "Booka Shade",
     songTitle: "Charlotte",
     views: 68000,
@@ -297,7 +297,7 @@ export const quickPicks: quickPicksProps[] = [
   {
     key: 10,
     size: "small",
-    imageUrl: "/assets/static/images/albumsImage/Boris1.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/Boris1.jpg",
     artist: "Boris Brejcha",
     songTitle: "Lieblingsmensch",
     views: 1200500,
@@ -306,7 +306,7 @@ export const quickPicks: quickPicksProps[] = [
   {
     key: 11,
     size: "small",
-    imageUrl: "/assets/static/images/albumsImage/EnricoSangiuliano.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/EnricoSangiuliano.jpg",
     artist: "Enrico Sangiuliano",
     songTitle: "In the Night",
     views: 560000,
@@ -316,7 +316,7 @@ export const quickPicks: quickPicksProps[] = [
     key: 12,
     size: "small",
     imageUrl:
-      "/assets/static/images/albumsImage/InTheDark-PurpleDiscoMachine.jpg",
+      "src/assets/static/images/albumsImage/InTheDark-PurpleDiscoMachine.jpg",
     artist: "Purple Disco Machine",
     songTitle: "In the Dark",
     views: 270000,
@@ -325,7 +325,7 @@ export const quickPicks: quickPicksProps[] = [
   {
     key: 13,
     size: "small",
-    imageUrl: "/assets/static/images/albumsImage/KyleWatson.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/KyleWatson.jpg",
     artist: "Kyle Watson",
     songTitle: "Shides",
     views: 3700,
@@ -334,7 +334,7 @@ export const quickPicks: quickPicksProps[] = [
   {
     key: 14,
     size: "small",
-    imageUrl: "/assets/static/images/albumsImage/Lorde-GreenLights.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/Lorde-GreenLights.jpg",
     artist: "Lorde",
     songTitle: "Green Lights",
     views: 1200500,
@@ -343,7 +343,7 @@ export const quickPicks: quickPicksProps[] = [
   {
     key: 15,
     size: "small",
-    imageUrl: "/assets/static/images/albumsImage/Pryda-Murani.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/Pryda-Murani.jpg",
     artist: "Pryda",
     songTitle: "Muranyi",
     views: 560000,
@@ -352,7 +352,7 @@ export const quickPicks: quickPicksProps[] = [
   {
     key: 16,
     size: "small",
-    imageUrl: "/assets/static/images/albumsImage/Lastlings-NoTime.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/Lastlings-NoTime.jpg",
     artist: "Lastlings",
     songTitle: "No Time",
     views: 27000000,
@@ -364,7 +364,7 @@ export const recomendedAlbums = [
   {
     key: 1,
     imageUrl:
-      "/assets/static/images/albumsImage/3b02630be867dbdbd629e9d32572a9b5.png",
+      "src/assets/static/images/albumsImage/3b02630be867dbdbd629e9d32572a9b5.png",
     isAlbum: true,
     artist: "Calvin Harris",
     playlistName: "Can't Stop the Feeling!",
@@ -373,7 +373,7 @@ export const recomendedAlbums = [
   {
     key: 2,
     imageUrl:
-      "/assets/static/images/albumsImage/43e62ad21860d99cd29386c472e3d49a.png",
+      "src/assets/static/images/albumsImage/43e62ad21860d99cd29386c472e3d49a.png",
     isAlbum: true,
     artist: "Various",
     playlistName: "Deep City Electro Collection",
@@ -382,7 +382,7 @@ export const recomendedAlbums = [
   {
     key: 3,
     imageUrl:
-      "/assets/static/images/albumsImage/758c2683a632d5521796b21be90d9541.png",
+      "src/assets/static/images/albumsImage/758c2683a632d5521796b21be90d9541.png",
     isAlbum: true,
     artist: "Ania Szarmach",
     playlistName: "Shades of Love",
@@ -390,7 +390,7 @@ export const recomendedAlbums = [
   },
   {
     key: 4,
-    imageUrl: "/assets/static/images/albumsImage/Lastlings-NoTime.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/Lastlings-NoTime.jpg",
     isAlbum: true,
     artist: "Lastlings",
     playlistName: "In the Space",
@@ -398,7 +398,7 @@ export const recomendedAlbums = [
   },
   {
     key: 5,
-    imageUrl: "/assets/static/images/albumsImage/Andromeda-Gorillaz.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/Andromeda-Gorillaz.jpg",
     isAlbum: true,
     artist: "Gorillaz",
     playlistName: "Demon Days",
@@ -406,7 +406,7 @@ export const recomendedAlbums = [
   },
   {
     key: 6,
-    imageUrl: "/assets/static/images/albumsImage/KREAM-Pressure.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/KREAM-Pressure.jpg",
     isAlbum: true,
     artist: "KREAM",
     playlistName: "Pressure",
@@ -415,7 +415,7 @@ export const recomendedAlbums = [
   {
     key: 7,
     imageUrl:
-      "/assets/static/images/albumsImage/InTheDark-PurpleDiscoMachine.jpg",
+      "src/assets/static/images/albumsImage/InTheDark-PurpleDiscoMachine.jpg",
     isAlbum: true,
     artist: "Purple Disco Machine",
     playlistName: "Exotica",
@@ -423,7 +423,7 @@ export const recomendedAlbums = [
   },
   {
     key: 8,
-    imageUrl: "/assets/static/images/albumsImage/FREYA-Ruback.jpg",
+    imageUrl: "src/assets/static/images/albumsImage/FREYA-Ruback.jpg",
     isAlbum: true,
     artist: "FREYA",
     playlistName: "Ruback",
